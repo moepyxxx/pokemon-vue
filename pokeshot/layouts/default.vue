2fbf12912dd1e71af53c66528fe86824b7b47661
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2021-12-31 14:08:16 +0900

    asyncDataで初期データを引っ張れるようにした

--- pokeshot/layouts/default.vue
+++ pokeshot/layouts/default.vue
@@ -1,118 +1,71 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app>
+
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
+      color="white"
+      elevation="3"
       app
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
+
+    <div
+      class="d-flex justify-end text-right"
+    >
+      <div
+        class="pa-2"
+        v-for="(navi, i) in navis"
+        :key="i"
       >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
+        <v-icon
+          medium
+          color="blue lighten-1"
+        >
+          mdi-{{ navi.icon }}
+        </v-icon>
+        <a :href="navi.linkPath">{{ navi.label }}</a>
+      </div>
+    </div>
+
     </v-app-bar>
+
     <v-main>
-      <v-container>
-        <Nuxt />
+
+      <v-container fluid>
+        <router-view />
       </v-container>
     </v-main>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+
+    <v-footer app>
+      <!-- -->
     </v-footer>
   </v-app>
 </template>
 
-<script>
-export default {
+
+<script lang="ts">
+import Vue from 'vue'
+
+type DataType = {
+ navis: {
+   linkPath: string;
+   label: string;
+   icon: string;
+ }[]
+}
+
+export default Vue.extend({
   name: 'DefaultLayout',
-  data () {
+  data (): DataType {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      navis: [{
+        linkPath: '/',
+        label: 'ホーム',
+        icon: 'home'
+      }, {
+        linkPath: 'search',
+        label: 'ポケモンを探しにいく',
+        icon: 'crosshairs'
+      }]
     }
   }
-}
+})
 </script>
