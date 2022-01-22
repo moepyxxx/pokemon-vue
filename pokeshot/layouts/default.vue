7015b0bc2f72e88e44099ec5310549bc13160952
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-01-22 16:44:58 +0900

    とりあえずコミット

--- pokeshot/layouts/default.vue
+++ pokeshot/layouts/default.vue
@@ -1,42 +1,12 @@
 <template>
   <v-app>
 
-    <v-app-bar
-      color="white"
-      elevation="3"
-      app
-    >
-
-    <div
-      class="d-flex justify-end text-right"
-    >
-      <div
-        class="pa-2"
-        v-for="(navi, i) in navis"
-        :key="i"
-      >
-        <v-icon
-          medium
-          color="blue lighten-1"
-        >
-          mdi-{{ navi.icon }}
-        </v-icon>
-        <a :href="navi.linkPath">{{ navi.label }}</a>
-      </div>
-    </div>
-
-    </v-app-bar>
-
     <v-main>
-
       <v-container fluid>
         <router-view />
       </v-container>
     </v-main>
 
-    <v-footer app>
-      <!-- -->
-    </v-footer>
   </v-app>
 </template>
 
