51d9fe6d21b29469c060b1dc69bc0efacb70d1d5
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-01-29 13:13:09 +0900

    TOPを修正した

--- pokeshot/pages/index.vue
+++ pokeshot/pages/index.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="align-center">
-    <h1>ポケモンを探してスクショしましょう！</h1>
-    <br />
-    <br />
-    <p>ポケモンを探しにいこう</p>
-
-    <div v-for="(region, i) in regions" :key="i">
-      <NuxtLink :to="'/region/' + region.id">{{ region.name }}</NuxtLink>
-    </div>
+    <h1>ポケモンをはじめましょう！</h1>
+    <NuxtLink to="start">ポケモンをはじめる</NuxtLink>
 
   </div>
 </template>
@@ -16,21 +10,7 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
-  // async asyncData({ $axios }) {
-  //   const POKE_API_REGION_URL = "https://pokeapi.co/api/v2/region";
-  //   const { results } = await $axios.$get(`${POKE_API_REGION_URL}?limit=3`);
-  //   const urls: string[] = results.map(result => result.url);
-  //   const regions: TRegion[] = await Promise.all(urls.map(async (url) => {
-  //     const region = await $axios.$get(url);
-      
-  //     return {
-  //       name: region.names[0].name,
-  //       id: Number(region.id)
-  //     }
-  //   }));
-  //   return { regions };
-  // },
+  name: 'IndexPage'
 })
 
 </script>
