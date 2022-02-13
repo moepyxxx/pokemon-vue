e7773d27a0e75cf7f481d3bd59d61a5b611322f1
moepyxxx <sjpy_xx_1007@docomo.ne.jp>
2022-02-13 17:16:30 +0900

    主人公の設定を入れてゲームスタートできるようにした

--- pokeshot/pages/start.vue
+++ pokeshot/pages/start.vue
@@ -1,19 +1,78 @@
 <template>
   <Screen>
-    <div v-if="status === 'selectPokemon'">
-      <div v-for="pokemon in pokemons" :key="pokemon.id">
-        <button @click="changeSelectPokemon(pokemon)">{{ pokemon.base.name }}</button>
+
+    <div v-if="status === 'greeting' || status === 'getFirstPokemon'" :class="status">
+      <img src="~/assets/img/scenario/odamaki.png" alt="オダマキ博士">
+    </div>
+
+    <div v-else-if="status === 'selectGender'" :class="status">
+      <div v-for="(gender, index) in genders" :key="index">
+        <button @click="registerGender(gender)">
+          <img
+            :src="require(`@/assets/img/scenario/${gender.english}.png`)"
+            :alt="gender.japanese"
+          />
+          <p>{{ gender.japanese }}</p>
+        </button>
       </div>
     </div>
-    <div v-else-if="status === 'confirm'">
-      <p>あなたが選んだポケモン：{{ selectPokemon.base.name }}</p>
+
+    <div v-else-if="status === 'registerName'" :class="status">
+      <div class="selected">
+        <img
+          :src="require(`@/assets/img/scenario/${selectedGender.english}.png`)"
+          :alt="selectedGender.japanese"
+        />
+        <p>{{ selectedGender.japanese }}</p>
+      </div>
+      <div class="form">
+        <input type="text" placeholder="5文字いないのなまえ" :v-model="heroName" />
+        <button @click="registerHeroName">これで決定</button>
+      </div>
     </div>
+
+    <div v-else-if="status === 'selectPokemon'" :class="status">
+      <div v-for="pokemon in pokemons" :key="pokemon.base.id">
+        <button @click="changeSelectPokemon(pokemon)">
+          <img
+            :src="require(`@/assets/img/scenario/pokemon/${pokemon.base.id}.png`)"
+            :alt="pokemon.base.name"
+          />
+          <p>{{ pokemon.base.name }}</p>
+        </button>
+      </div>
+    </div>
+    
+    <div v-else-if="status === 'confirm'" class="selectPokemon">
+      <img
+        :src="require(`@/assets/img/scenario/pokemon/${selectPokemon.base.id}.png`)"
+        :alt="selectPokemon.base.name"
+      >
+      <p>{{ selectPokemon.base.name }}</p>
+    </div>
+
+    <div v-else-if="status === 'registerNickname'" class="selectPokemon">
+      <div class="selected">
+        <img
+          :src="require(`@/assets/img/scenario/pokemon/${selectPokemon.base.id}.png`)"
+          :alt="selectPokemon.base.name"
+        >
+        <p>{{ selectPokemon.base.name }}</p>
+      </div>
+      <div class="form">
+        <input type="text" placeholder="5文字いないのなまえ" :v-model="pokemonNickname" />
+        <button @click="fixFirstPokemon(true)">これで決定</button>
+        <button @click="fixFirstPokemon(false)">ニックネームはつけない</button>
+      </div>
+    </div>
+
     <Serif
       :serifs="serifs"
       :isQuestion="isQuestion"
       @next="next"
       @back="back"
     />
+
   </Screen>
 </template>
 
@@ -28,11 +87,15 @@ import { pokemonSelectableInFirst } from "../datas/pokemonSelectableInFirst"
 import IWildPokemon from '../interface/IWildPokemon'
 import IHandPokemon from '../interface/IHandPokemon'
 import HeroCurrent, { IHeroCurrent } from '~/store/modules/heroCurrent';
+import { THeroGender } from '~/store/modules/hero';
+import Hero from '~/store/modules/hero';
+
 
 const handPokemonsModule = getModule(HandPokemons);
 const heroCurrentModule = getModule(HeroCurrent);
+const heroModule = getModule(Hero);
 
-type TStatus = 'greeting' | 'selectPokemon' | 'confirm' | 'getFirstPokemon' | 'gotoField';
+type TStatus = 'greeting' | 'selectGender' | 'registerName' | 'selectPokemon' | 'confirm' | 'registerNickname' | 'getFirstPokemon' | 'gotoField';
 
 @Component({
   components: {
@@ -56,21 +119,65 @@ type TStatus = 'greeting' | 'selectPokemon' | 'confirm' | 'getFirstPokemon' | 'g
 
 export default class StartPage extends Vue {
 
-  pokemons?: IHandPokemon[];
-
-  nickname: string = '';
+  pokemons: IHandPokemon[] = [];
+  pokemonNickname: string = '';
   selectPokemon: IWildPokemon|null = null;
+
+  genders: THeroGender[] = [{
+    english: 'girl',
+    japanese: 'おんなのこ'
+  }, {
+    english: 'boy',
+    japanese: 'おとこのこ'
+  }]
+  selectedGender: THeroGender = {
+    english: 'girl',
+    japanese: 'おんなのこ'
+  };
+
+  heroName: string = '';
+
   isFixFirstPokemon: boolean = false;
+
   status: TStatus = 'greeting';
+
   isQuestion: boolean = false;
   serifs: string[] = [
     'こんにちは ぼくのなまえは オダマキ博士',
-    'これから きみのあいぼうを えらんで たびに でよう',
-    'きみは どの ポケモンを えらぶかな？',
+    'これから きみのあいぼうを えらんで たびに でよう'
   ]
+
+  registerGender(gender: THeroGender) {
+    this.selectedGender = gender;
+    heroModule.registerHero({
+      ...heroModule.hero,
+      gender
+    })
+    this.serifs.splice(0);
+    this.status = 'registerName';
+  }
+
+  registerHeroName() {
+    heroModule.registerHero({
+      ...heroModule.hero,
+      name: this.heroName
+    })
+    this.serifs.splice(0);
+    this.status = 'selectPokemon';
+  }
+
+  registerPokemonNickname() {
+    heroModule.registerHero({
+      ...heroModule.hero,
+      name: this.heroName
+    })
+    this.serifs.splice(0);
+    this.status = 'selectPokemon';
+  }
 
   changeSelectPokemon(pokemon: IWildPokemon) {
     this.selectPokemon = pokemon;
+    this.serifs.splice(0);
     this.status = 'confirm';
   }
 
@@ -78,11 +185,11 @@ export default class StartPage extends Vue {
     switch (this.status) {
       case 'greeting':
         this.serifs.splice(0);
-        this.status = 'selectPokemon';
+        this.status = 'selectGender';
         break;
       case 'confirm':
         this.serifs.splice(0);
-        this.status = 'getFirstPokemon';
+        this.status = 'registerNickname';
         break;
       case 'getFirstPokemon':
         this.serifs.splice(0);
@@ -101,12 +208,17 @@ export default class StartPage extends Vue {
     }
   }
 
-  fixFirstPokemon() {
+  fixFirstPokemon(isNickname: boolean) {
     if (!this.selectPokemon) return;
 
-    const onHandPokemon: IHandPokemon = this.$Poke.getHandPokemon(this.selectPokemon, this.nickname);
+    this.pokemonNickname = isNickname ? this.pokemonNickname : this.selectPokemon.base.name;
+
+    const onHandPokemon: IHandPokemon = this.$Poke.getHandPokemon(this.selectPokemon, this.pokemonNickname);
     handPokemonsModule.addToOnHandPokemon(onHandPokemon);
     this.isFixFirstPokemon = true;
+
+    this.serifs.splice(0);
+    this.status = 'getFirstPokemon';
   }
 
   setFirstHeroCurrent() {
@@ -124,14 +236,26 @@ export default class StartPage extends Vue {
     switch(status) {
       case 'greeting':
         break;
+      case 'selectGender':
+        this.serifs.push('そのまえに まずはあなたのことを教えてもらうよ。 あなたは おとこのこ？ それともおんなのこ？');
+        break;
+      case 'registerName':
+        this.serifs.push('それではつぎに あなたの名前を おしえてね');
+        break;
+      case 'selectPokemon':
+        this.serifs.push(`${heroModule.hero.name} 次はいよいよ きみのさいしょのポケモンを えらぶときだ。 さあ どのポケモンにする？`);
+        break;
       case 'confirm':
-        this.serifs.push(`ほんとうに この${ this.selectPokemon ? this.selectPokemon.base.name : 'ダミー' }で良いのかな？`);
+        this.serifs.push(`ほんとうに この${ this.selectPokemon!.base.name }で良いのかな？`);
         this.isQuestion = true;
         break;
-      case 'getFirstPokemon':
+      case 'registerNickname':
+        this.serifs.splice(0);
         this.isQuestion = false;
-        this.fixFirstPokemon();
-        this.serifs.push(`あなたは さいしょの あいぼうとして ${ this.selectPokemon ? this.selectPokemon.base.name : 'ダミー' } を選んだね`);
+        this.serifs.push(`${this.selectPokemon!.base.name}に ニックネームをつけてみるかい？`);
+        break;
+      case 'getFirstPokemon':
+        this.serifs.push(`あなたは さいしょの あいぼうとして ${ this.selectPokemon!.base.name } を選んだね`);
         this.serifs.push(`それでは 旅に でてみよう！`);
         this.setFirstHeroCurrent();
         break;
---
+++ pokeshot/store/modules/hero.ts
@@ -0,0 +1,32 @@
+import { VuexModule, Module, Mutation } from 'vuex-module-decorators' 
+import store from "@/store/store"
+
+export type THeroGender = {
+  english: string,
+  japanese: string
+};
+
+export interface IHero {
+  gender: THeroGender;
+  name: string;
+}
+
+@Module({ dynamic: true, store, name: "hero" })
+export default class HeroCurrent extends VuexModule {
+  hero: IHero = {
+    gender: {
+      english: 'girl',
+      japanese: 'おんなのこ'
+    },
+    name: ''
+  }
+
+  get current() {
+    return this.hero;
+  }
+
+  @Mutation
+  public registerHero(hero: IHero) {
+    this.hero = hero;
+  }
+}
